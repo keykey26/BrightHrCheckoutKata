96ec02f4c1dc56ea1bf077ed4ea201e7e531b30f
Ben Sheridan <keykey26@gmail.com>
2025-02-15 17:08:08 +0000

    Added tests for price calcalation

--- CheckoutTests/Checkouttests.cs
+++ CheckoutTests/Checkouttests.cs
@@ -52,5 +52,62 @@ namespace CheckoutTests
 
             Assert.IsFalse(checkoutService.Products.Any(), "Scan has added unknown product");
         }
+
+        [TestMethod]
+        public void GetTotalPriceTestOneProduct()
+        {
+            _productService.GetProduct(Arg.Any<string>()).Returns(new Product("A", 50, 3, 130));
+
+            var checkoutService = new CheckoutService(_productService);
+
+            checkoutService.Scan("A");
+
+            Assert.IsFalse(checkoutService.GetTotalPrice() == 50, "Price for 1 product is wrong");
+        }
+
+        [TestMethod]
+        public void GetTotalPriceTestMutipleProduct()
+        {
+            _productService.GetProduct("A").Returns(new Product("A", 50, 3, 130));
+            _productService.GetProduct("B").Returns(new Product("B", 30, 2, 45));
+            _productService.GetProduct("C").Returns(new Product("C", 20, 0, 0));
+
+            var checkoutService = new CheckoutService(_productService);
+
+            checkoutService.Scan("A");
+            checkoutService.Scan("B");
+            checkoutService.Scan("C");
+
+            Assert.IsFalse(checkoutService.GetTotalPrice() == 100, "Price for mutiple products is wrong");
+        }
+
+        [TestMethod]
+        public void GetTotalPriceTestSpecialOffer()
+        {
+            _productService.GetProduct(Arg.Any<string>()).Returns(new Product("A", 50, 3, 130));
+
+            var checkoutService = new CheckoutService(_productService);
+
+            checkoutService.Scan("A");
+            checkoutService.Scan("A");
+            checkoutService.Scan("A");
+
+            Assert.IsFalse(checkoutService.GetTotalPrice() == 130, "Price for mutiple products is wrong");
+        }
+
+        [TestMethod]
+        public void GetTotalPriceTestSpecialOfferWithExtra()
+        {
+            _productService.GetProduct(Arg.Any<string>()).Returns(new Product("A", 50, 3, 130));
+
+            var checkoutService = new CheckoutService(_productService);
+
+            checkoutService.Scan("A");
+            checkoutService.Scan("A");
+            checkoutService.Scan("A");
+            checkoutService.Scan("A");
+
+            Assert.IsFalse(checkoutService.GetTotalPrice() == 180, "Price for mutiple products is wrong");
+        }
     }
 }
